fb0e694b9d64bcfd7f1ce132fe49d168d5b0508f
PacoMoya <160237221+MoyaPaco@users.noreply.github.com>
2024-03-08 11:08:23 +0100

    Squashed commit of the following:
    commit 50ff8272327123982e555191add2094e071bb760
    Author: PacoMoya <160237221+MoyaPaco@users.noreply.github.com>
    Date:   Fri Mar 8 11:06:09 2024 +0100
    
        nueva funcionalidad codigo
    
    commit 8158e429df7e1a62c97ee41d42ed1b5a8b422b70
    Author: PacoMoya <160237221+MoyaPaco@users.noreply.github.com>
    Date:   Fri Mar 8 11:02:25 2024 +0100
    
        aplicacion

---
+++ NuevaFuncionalidad.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class NuevaFuncionalidad{
+
+    public static void main(String[] main){
+
+    Scanner reader = new Scanner(System.in);
+
+    int numero1 = 0;
+    int numero2 = 0;
+    System.out.println("Introduce el primer número:");
+    numero1 = reader.nextInt();
+    System.out.println("Introduce el segundo número:");
+    numero2 = reader.nextInt();
+    }
+}
